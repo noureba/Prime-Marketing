2e034c87a4397a7b7a13fc1dafb7c2558f31d04e
Noure DDine <94680573+noureba@users.noreply.github.com>
2025-04-05 12:24:34 +0000

    create mobile menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 require("./styles/globals.css");
+
+const barMenuBtn = document.querySelector("#barMenu");
+const barMenu = document.getElementById("menu");
+const overlay = document.querySelector(".overlay");
+const closeMenuBtn = document.querySelectorAll(".closeMenu");
+const menuContainer = document.querySelector(".menuContainer");
+
+barMenuBtn.addEventListener("click", () => {
+  barMenu.style.transform = "translateX(0)";
+  barMenu.style.transition = "transform 0.3s ease-in-out";
+  menuContainer.style.display = "block";
+  overlay.style.display = "block";
+});
+
+closeMenuBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    barMenu.style.transform = "translateX(-100%)";
+    barMenu.style.transition = "transform 0.3s ease-in-out";
+    menuContainer.style.display = "none";
+    overlay.style.display = "none";
+  });
+});
