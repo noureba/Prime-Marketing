0bd0ca088237af310f8ec96d9a8a050ef4b3dae7
Noure DDine <94680573+noureba@users.noreply.github.com>
2025-04-05 14:46:57 +0000

    make header sticky and add scroll button top

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const barMenu = document.getElementById("menu");
 const overlay = document.querySelector(".overlay");
 const closeMenuBtn = document.querySelectorAll(".closeMenu");
 const menuContainer = document.querySelector(".menuContainer");
+const header = document.querySelector("header");
+const scrollTopBtn = document.querySelector(".scrollTopBtn");
 
 barMenuBtn.addEventListener("click", () => {
   barMenu.style.transform = "translateX(0)";
@@ -20,4 +22,17 @@ closeMenuBtn.forEach((btn) => {
     menuContainer.style.display = "none";
     overlay.style.display = "none";
   });
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) {
+    header.style.position = "fixed";
+    header.style.transition = "header 1s ease-in-out";
+    scrollTopBtn.style.display = "block";
+    scrollTopBtn.style.transition = "scrollTopBtn 1s ease-in-out";
+  }else{
+    header.style.position = "static";
+    scrollTopBtn.style.display = "none";
+
+  }
 });
